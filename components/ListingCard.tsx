249400672c5f7696d4796ae909b11fcef5ef54d4
synthful <59634395+kyle1373@users.noreply.github.com>
2024-07-30 04:40:46 -0700

    Update ListingCard.tsx

--- components/ListingCard.tsx
+++ components/ListingCard.tsx
@@ -10,6 +10,11 @@ type Listing = {
   server: string;
   created_at: string;
   description: null;
+  world: string;
+  location: string;
+  can_discord_contact_when_offline: boolean;
+  is_closed: boolean;
+  oncehuman_username: string;
 };
 
 type UserInfo = {
@@ -19,7 +24,6 @@ type UserInfo = {
   last_online: string;
   discord_name: string;
   discord_image: string;
-  oncehuman_username: null;
 };
 
 type ItemAsking = {
@@ -82,54 +86,66 @@ const ListingCard = ({ entry }: ListingCardProps) => {
 
     return <span className="italic">({formattedDate})</span>;
   };
+
   const sellingItem =
     entry.items_selling.length > 0 ? entry.items_selling[0] : null;
 
   return (
     <>
       <Link href={`/listing/${entry.listing.id}`}>
-        <div className="flex bg-sky-900 rounded-md">
-          <div className="flex-shrink-0 px-4 py-2 justify-center items-center text-center">
-            <img
-              className="w-20 h-20 border-2 rounded-sm border-sky-950"
-              src={LINKS.baseImagePath + sellingItem.s3_image_path}
-            />
-            <h1 className="text-xs mt-1">{sellingItem.amount} per order</h1>
-            <h1 className="text-xs mt-1">{sellingItem.total_stock} in stock</h1>
-          </div>
-          <div className="flex-grow pt-1 pr-3">
-            <h1 className="font-bold text-lg">{sellingItem.name}</h1>
-            <h1 className="font-normal text-xs">
-              {entry.user_info.oncehuman_username ||
-                entry.user_info.discord_name}{" "}
-              <DateComponent timestamp={entry.user_info.last_online} />
-            </h1>
-            <div className="flex flex-col items-start mt-1">
-              <div className="flex justify-start mt-2">
-                <IoMdSwap size={30} className="mt-1" />
-                {entry.items_asking.map((item, index) => (
-                  <>
-                    <div
-                      key={index}
-                      className="flex flex-col items-center mx-2"
-                    >
-                      <img
-                        className="w-10 h-10 border rounded-sm border-sky-950"
-                        src={LINKS.baseImagePath + item.s3_image_path}
-                      />
-                      <h1 className="text-center items-center">
-                        X {item.amount}
-                      </h1>
-                    </div>
-                    {index < entry.items_asking.length - 1 && (
-                      <h1 key={index} className="mt-1">
-                        or
-                      </h1>
-                    )}
-                  </>
-                ))}
+        <div className="bg-sky-950 border border-sky-700 rounded-md max-w-[600px] hover:bg-sky-900">
+          <div className="flex">
+            <div className="flex-shrink-0 px-4 py-2 justify-center items-center text-center font-bold">
+              <div className="relative border-2 rounded-sm border-sky-950">
+                <img
+                  className=" border-sky-950 w-20 h-20"
+                  src={LINKS.baseImagePath + sellingItem.s3_image_path}
+                />
+                <div className="absolute bottom-0 right-0 left-0 px-2 py-1 bg-[#00000085] text-white text-sm">
+                  X {sellingItem.amount}
+                </div>
+              </div>
+              <h1 className="text-xs mt-1">
+                {sellingItem.total_stock} in stock
+              </h1>
+            </div>
+            <div className="flex-grow pt-1 pr-3">
+              <h1 className="font-bold text-lg">{sellingItem.name}</h1>
+              <h1 className="font-normal text-xs">
+                {entry.listing.oncehuman_username}{" "}
+                <DateComponent timestamp={entry.user_info.last_online} />
+              </h1>
+              <div className="flex flex-col items-start mt-2 mb-1">
+                <div className="flex justify-start">
+                  <IoMdSwap size={30} className="mt-2" />
+                  {entry.items_asking.map((item, index) => (
+                    <>
+                      <div
+                        key={index}
+                        className="flex flex-col items-center mx-2"
+                      >
+                        <img
+                          className="w-10 h-10 border rounded-sm border-sky-950"
+                          src={LINKS.baseImagePath + item.s3_image_path}
+                        />
+                        <h1 className="text-center items-center text-xs font-bold">
+                          X {item.amount}
+                        </h1>
+                      </div>
+                      {index < entry.items_asking.length - 1 && (
+                        <h1 className="mt-2">or</h1>
+                      )}
+                    </>
+                  ))}
+                </div>
               </div>
             </div>
+          </div>
+          <div className="flex flex-wrap justify-between px-4 py-2 border-t border-sky-700 text-sm break-all text-sky-500">
+            <h1>
+              World {entry.listing.world}: {entry.listing.location}
+            </h1>
+            <h1 className="pl-2">{entry.listing.server}</h1>
           </div>
         </div>
       </Link>
